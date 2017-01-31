a43b7ed81a87ed9495e516f0272dc12e33b49064
amitt4 <amitt4@chetu.com>
2017-01-31 19:34:24 +0530

    Add Himachal Pradesh

--- app/app.js
+++ app/app.js
@@ -22,6 +22,7 @@ app.controller('MainCtrl', function ($scope, $element) {
       { id: 'NE', text: 'Nebraska' },
       { id: 'NM', text: 'New Mexico' },
       { id: 'JK', text: 'Jammu & Kashmir'}
+      {id: 'HP', text: 'Himachal Pradesh'}
 
      
     ]},
